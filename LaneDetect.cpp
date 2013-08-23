481dfc63d19f04ddf5478441e80904b2d7e1ccc7
turtlebot <turtlebot@turtlebot.(none)>
2013-08-22 21:53:45 -0400

    update

--- LaneDetect.cpp
+++ LaneDetect.cpp
@@ -1,6 +1,8 @@
 /*------------------------------------------------------------------------------------------*\
-   This file contains material supporting chapter 7 of the cookbook:  
-   Computer Vision Programming using the OpenCV Library. 
+   Lane Detection
+
+   General idea and some code modified from:
+   chapter 7 of Computer Vision Programming using the OpenCV Library. 
    by Robert Laganiere, Packt Publishing, 2011.
 
    This program is free software; permission is hereby granted to use, copy, modify, 
@@ -12,266 +14,177 @@
    The author disclaims all warranties with regard to this software, any use, 
    and any consequent failure, is purely the responsibility of the user.
  
-   Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
+   Copyright (C) 2013 Jason Dorweiler, www.transistor.io
+
+
+Notes: 
+
+	Can we compare the Hough and HoughP images? 
+	eg: Bitwise and on Hough and contour image
+	    Bitwise and on HoughP and result of previous image. need to write HoughP to blank Mat of same size
+	    Filter for angles based on theta, there should be a ~pi/2 angle on the otherside
+	    of the road.  If there isn't one then drop that line for the list. 
+
+	Add up number on lines that are found within a threshold of a given rho,theta and 
+	use that to determine a score.  Only lines with a good enough score are kept. 
+
+	Calculation for the distance of the car from the center.  This should also determine
+	if the road in turning.  We might not want to be in the center of the road for a turn. 
+
+	The minimum vote used in the Hough filter is very sensitive to the image used.  It might be a 
+	good idea to set the algo up to reprocess the image if the bitwise adding of the two hough
+	filters does not produce at least two lines.
+	
+	Several other parameters can be played with: min vote on houghp, line distance and gap.  Some
+	type of feed back loop might be good to self tune these parameters. 
+
+	We are still finding the Road, i.e. both left and right lanes.  we Need to set it up to find the
+	yellow divider line in the middle. 
 \*------------------------------------------------------------------------------------------*/
 
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <vector>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
+#include <stdio.h>
 #include "linefinder.h"
-#include "edgedetector.h"
+//#include "edgedetector.h"
 
 #define PI 3.1415926
 
-int main(int argc, char* argv[])
-{
-	// Read input image
-	cv::Mat image= cv::imread(argv[1]);
-	if (!image.data)
-		return 0; 
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+	int houghVote = 200;
+	string arg = argv[1];
+	bool showSteps = argv[2];
+
+	string window_name = "Processed Video";
+	namedWindow(window_name, CV_WINDOW_KEEPRATIO); //resizable window;
+	//VideoCapture capture(arg);
+
+	//if (!capture.isOpened()) //if this fails, try to open as a video camera, through the use of an integer param
+        //	{capture.open(atoi(arg.c_str()));}
+
+        Mat image=imread(argv[1]);
+        while (1)
+        {/*
+            //	capture >> image;
+            if (image.empty())
+                break;
+            	Mat gray;
+            	cvtColor(image,gray,CV_RGB2GRAY);
+            	vector<string> codes;
+            	Mat corners;
+            	findDataMatrix(gray, codes, corners);
+            	drawDataMatrixCodes(image, codes, corners);*/
 
     // Display the image
-	cv::namedWindow("Original Image");
-	cv::imshow("Original Image",image);
+	if(showSteps){
+		namedWindow("Original Image");
+		imshow("Original Image",image);
+		imwrite("original.bmp", image);
+	}
 
-	// Compute Sobel
-	EdgeDetector ed;
-	ed.computeSobel(image);
+   // Canny algorithm
+	Mat contours;
+	Canny(image,contours,50,350);
+	Mat contoursInv;
+	threshold(contours,contoursInv,128,255,THRESH_BINARY_INV);
 
-    // Display the Sobel orientation
-	cv::namedWindow("Sobel (orientation)");
-	cv::imshow("Sobel (orientation)",ed.getSobelOrientationImage());
-	cv::imwrite("ori.bmp",ed.getSobelOrientationImage());
+   // Display Canny image
+	if(showSteps){
+		namedWindow("Contours");
+		imshow("Contours",contoursInv);
+		imwrite("contours.bmp", contoursInv);
+	}
 
-    // Display the Sobel low threshold
-	cv::namedWindow("Sobel (low threshold)");
-	cv::imshow("Sobel (low threshold)",ed.getBinaryMap(125));
-
-    // Display the Sobel high threshold
-	cv::namedWindow("Sobel (high threshold)");
-	cv::imshow("Sobel (high threshold)",ed.getBinaryMap(350));
-
-	// Apply Canny algorithm
-	cv::Mat contours;
-	cv::Canny(image,contours,125,350);
-	cv::Mat contoursInv;
-	cv::threshold(contours,contoursInv,128,255,cv::THRESH_BINARY_INV);
-
-    // Display the image of contours
-	cv::namedWindow("Canny Contours");
-	cv::imshow("Canny Contours",contoursInv);
-
-	// Create a test image
-	cv::Mat test(200,200,CV_8U,cv::Scalar(0));
-	cv::line(test,cv::Point(100,0),cv::Point(200,200),cv::Scalar(255));
-	cv::line(test,cv::Point(0,50),cv::Point(200,200),cv::Scalar(255));
-	cv::line(test,cv::Point(0,200),cv::Point(200,0),cv::Scalar(255));
-	cv::line(test,cv::Point(200,0),cv::Point(0,200),cv::Scalar(255));
-	cv::line(test,cv::Point(100,0),cv::Point(100,200),cv::Scalar(255));
-	cv::line(test,cv::Point(0,100),cv::Point(200,100),cv::Scalar(255));
-
-    // Display the test image
-	cv::namedWindow("Test Image");
-	cv::imshow("Test Image",test);
-	cv::imwrite("test.bmp",test);
-
-	// Hough tranform for line detection
-	std::vector<cv::Vec2f> lines;
-	cv::HoughLines(contours,lines,1,PI/180,150);
-
-	// Draw the lines
-	cv::Mat result(contours.rows,contours.cols,CV_8U,cv::Scalar(255));
+ /* 
+	Hough tranform for line detection with feedback
+	Increase by 25 for the next frame if we found some lines.  
+	This is so we don't miss other lines that may crop up in the next frame
+	but at the same time we don't want to start the feed back loop from scratch. 
+*/
+	std::vector<Vec2f> lines;
+	if (houghVote < 1 or lines.size() > 2){ // we lost all lines. reset 
+		houghVote = 200; 
+	}
+	else{ houghVote += 25;} 
+	while(lines.size() < 5 && houghVote > 0){
+		HoughLines(contours,lines,1,PI/180, houghVote);
+		houghVote -= 5;
+	}
+	std::cout << houghVote << "\n";
+	Mat result(contours.rows,contours.cols,CV_8U,Scalar(255));
 	image.copyTo(result);
 
-	std::cout << "Lines detected: " << lines.size() << std::endl;
-
-	std::vector<cv::Vec2f>::const_iterator it= lines.begin();
+   // Draw the limes
+	std::vector<Vec2f>::const_iterator it= lines.begin();
+	Mat hough(image.size(),CV_8U,Scalar(0));
 	while (it!=lines.end()) {
 
 		float rho= (*it)[0];   // first element is distance rho
 		float theta= (*it)[1]; // second element is angle theta
 		
-		if (theta < PI/4. || theta > 3.*PI/4.) { // ~vertical line
+		//if (theta < PI/20. || theta > 19.*PI/20.) { // filter theta angle to find lines with theta between 30 and 150 degrees (mostly vertical)
 		
 			// point of intersection of the line with first row
-			cv::Point pt1(rho/cos(theta),0);        
+			Point pt1(rho/cos(theta),0);        
 			// point of intersection of the line with last row
-			cv::Point pt2((rho-result.rows*sin(theta))/cos(theta),result.rows);
+			Point pt2((rho-result.rows*sin(theta))/cos(theta),result.rows);
 			// draw a white line
-			cv::line( result, pt1, pt2, cv::Scalar(255), 1); 
+			line( result, pt1, pt2, Scalar(255), 8); 
+			line( hough, pt1, pt2, Scalar(255), 8);
+		//}
 
-		} else { // ~horizontal line
-
-			// point of intersection of the line with first column
-			cv::Point pt1(0,rho/sin(theta));        
-			// point of intersection of the line with last column
-			cv::Point pt2(result.cols,(rho-result.cols*cos(theta))/sin(theta));
-			// draw a white line
-			cv::line( result, pt1, pt2, cv::Scalar(255), 1); 
-		}
-
-		std::cout << "line: (" << rho << "," << theta << ")\n"; 
-
+		//std::cout << "line: (" << rho << "," << theta << ")\n"; 
 		++it;
 	}
 
     // Display the detected line image
-	cv::namedWindow("Detected Lines with Hough");
-	cv::imshow("Detected Lines with Hough",result);
-
-	// Create LineFinder instance
+	if(showSteps){
+		namedWindow("Detected Lines with Hough");
+		imshow("Detected Lines with Hough",result);
+		imwrite("hough.bmp", result);
+	}
+   // Create LineFinder instance
 	LineFinder ld;
 
-	// Set probabilistic Hough parameters
-	ld.setLineLengthAndGap(100,20);
-	ld.setMinVote(5);
+   // Set probabilistic Hough parameters
+	ld.setLineLengthAndGap(60,10);
+	ld.setMinVote(4);
 
-	// Detect lines
-	std::vector<cv::Vec4i> li= ld.findLines(contours);
-	ld.drawDetectedLines(image);
-	cv::namedWindow("Detected Lines with HoughP");
-	cv::imshow("Detected Lines with HoughP",image);
+   // Detect lines
+	std::vector<Vec4i> li= ld.findLines(contours);
+	Mat houghP(image.size(),CV_8U,Scalar(0));
+	ld.drawDetectedLines(houghP);
 
-	std::vector<cv::Vec4i>::const_iterator it2= li.begin();
-	while (it2!=li.end()) {
-
-		std::cout << "(" << (*it2)[0] << ","<< (*it2)[1]<< ")-(" 
-			     << (*it2)[2]<< "," << (*it2)[3] << ")" <<std::endl;
-
-		++it2;
+	if(showSteps){
+		namedWindow("Detected Lines with HoughP");
+		imshow("Detected Lines with HoughP", houghP);
+		imwrite("houghP.bmp", houghP);
 	}
 
-	// Display one line
-	image= cv::imread(argv[1]);
-	int n=0;
-	cv::line(image, cv::Point(li[n][0],li[n][1]),cv::Point(li[n][2],li[n][3]),cv::Scalar(255),5);
-	cv::namedWindow("One line of the Image");
-	cv::imshow("One line of the Image",image);
+   // bitwise AND of the two hough images
+	bitwise_and(houghP,hough,houghP);
+	Mat houghPinv(image.size(),CV_8U,Scalar(0));
+	Mat dst(image.size(),CV_8U,Scalar(0));
+	threshold(houghP,houghPinv,150,255,THRESH_BINARY_INV); // threshold and invert to black lines
 
-	// Extract the contour pixels of the first detected line
-	cv::Mat oneline(image.size(),CV_8U,cv::Scalar(0));
-	cv::line(oneline, cv::Point(li[n][0],li[n][1]),cv::Point(li[n][2],li[n][3]),cv::Scalar(255),5);
-	cv::bitwise_and(contours,oneline,oneline);
-	cv::Mat onelineInv;
-	cv::threshold(oneline,onelineInv,128,255,cv::THRESH_BINARY_INV);
-	cv::namedWindow("One line");
-	cv::imshow("One line",onelineInv);
-
-	std::vector<cv::Point> points;
-
-	// Iterate over the pixels to obtain all point positions
-	for( int y = 0; y < oneline.rows; y++ ) {
-    
-		uchar* rowPtr = oneline.ptr<uchar>(y);
-    
-		for( int x = 0; x < oneline.cols; x++ ) {
-
-		    // if on a contour
-			if (rowPtr[x]) {
-
-				points.push_back(cv::Point(x,y));
-			}
-		}
-    }
+	image.copyTo(dst, houghPinv); // copy lines to image
 	
-	// find the best fitting line
-	cv::Vec4f line;
-	cv::fitLine(cv::Mat(points),line,CV_DIST_L2,0,0.01,0.01);
-
-	std::cout << "line: (" << line[0] << "," << line[1] << ")(" << line[2] << "," << line[3] << ")\n"; 
-
-	int x0= line[2];
-	int y0= line[3];
-	int x1= x0-200*line[0];
-	int y1= y0-200*line[1];
-	image= cv::imread(argv[1]);
-	cv::line(image,cv::Point(x0,y0),cv::Point(x1,y1),cv::Scalar(0),3);
-	cv::namedWindow("Estimated line");
-	cv::imshow("Estimated line",image);
-
-	// eliminate inconsistent lines
-	ld.removeLinesOfInconsistentOrientations(ed.getOrientation(),0.4,0.1);
-
-   // Display the detected line image
-	image= cv::imread(argv[1]);
-	ld.drawDetectedLines(image);
-	cv::namedWindow("Detected Lines (2)");
-	cv::imshow("Detected Lines (2)",image);
-
-	// Create a Hough accumulator
-	cv::Mat acc(200,180,CV_8U,cv::Scalar(0));
-
-	// Choose a point
-	int x=50, y=30;
-
-	// loop over all angles
-	for (int i=0; i<180; i++) {
-
-		double theta= i*PI/180.;
-
-		// find corresponding rho value 
-		double rho= x*cos(theta)+y*sin(theta);
-		int j= static_cast<int>(rho+100.5);
-
-		std::cout << i << "," << j << std::endl;
-
-		// increment accumulator
-		acc.at<uchar>(j,i)++;
-	}
-
-	cv::imwrite("hough1.bmp",acc*100);
-
-	// Choose a second point
-	x=30, y=10;
-
-	// loop over all angles
-	for (int i=0; i<180; i++) {
-
-		double theta= i*PI/180.;
-		double rho= x*cos(theta)+y*sin(theta);
-		int j= static_cast<int>(rho+100.5);
-
-		acc.at<uchar>(j,i)++;
-	}
-
-	cv::namedWindow("Hough Accumulator");
-	cv::imshow("Hough Accumulator",acc*200);
-	cv::imwrite("hough2.bmp",acc*200);
-/*
-	// Detect circles
-	image= cv::imread(argv[1]);
-	cv::GaussianBlur(image,image,cv::Size(5,5),1.5);
-	std::vector<cv::Vec3f> circles;
-	cv::HoughCircles(image, circles, CV_HOUGH_GRADIENT, 
-		2,   // accumulator resolution (size of the image / 2) 
-		50,  // minimum distance between two circles
-		200, // Canny high threshold 
-		100, // minimum number of votes 
-		25, 100); // min and max radius
-
-	std::cout << "Circles: " << circles.size() << std::endl;
+	std::stringstream stream;
+	stream << "Lines Segments: " << lines.size();
 	
-	// Draw the circles
-	image= cv::imread(argv[1]);
-	std::vector<cv::Vec3f>::const_iterator itc= circles.begin();
-	
-	while (itc!=circles.end()) {
-		
-	  cv::circle(image, 
-		  cv::Point((*itc)[0], (*itc)[1]), // circle centre
-		  (*itc)[2], // circle radius
-		  cv::Scalar(255), // color 
-		  2); // thickness
-		
-	  ++itc;	
+	putText(dst, stream.str(), Point(10,image.rows-10), 2, 0.8, Scalar(0,0,255),0);
+        imshow(window_name, dst); 
+	imwrite("processed.bmp", dst);
+	char key = (char) waitKey(2);
+	lines.clear();
 	}
-
-	cv::namedWindow("Detected Circles");
-	cv::imshow("Detected Circles",image);
-*/
-	cv::waitKey();
-	return 0;
 }
+
+
+
+
